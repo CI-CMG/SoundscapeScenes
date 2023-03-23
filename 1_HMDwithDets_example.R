edc1a90f402d7fc103dd06a4fdd7cc1f7d3840e4
mfmckenna <megan.mckenna@gmail.com>
2023-03-23 14:52:54 -0700

    notes on params

--- 1_HMDwithDets_example.R
+++ 1_HMDwithDets_example.R
@@ -1,11 +1,17 @@
-# 1) Lable and Integrate 1-min hybrid-milli-decade data with event based detections
-# eg. bio_speciesCode
+# 1) Label and Integrate 1-min hybrid-milli-decade data with event based detections
+# eg. bio_species Code
 
 # Event detections: detection periods with start and end time
 
 # 2) Label into specific predefined acoustic scene categories
+# low-frequency- frequency range 100- 500 Hz (Kait is 500 Hz, so limiting, why not 1 or 2 kHz)
+# bio is very generic
+# bio + anthro
+# anthro
+# unknown?
 
-# 2) Label with context variables: ice, wind, AIS
-
-
+# 3) Label with context variables: ice (1-4), wind (1 or 2), AIS  (bring in once acoustic scene analysis)
+# this is a generic need in community- is labeling data- wind and event-based detections?
+# axiom help with data integration- no just visualization + data products
+# show example of wind and what I needed do to get to this
 
