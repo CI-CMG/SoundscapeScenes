f9e2fe6872574af346f10c1d583092cadfc3b7ba
mfmckenna <megan.mckenna@gmail.com>
2024-11-19 16:22:57 -0800

    gantt works, not map

--- GCPaudio_NRS.R
+++ GCPaudio_NRS.R
@@ -5,6 +5,11 @@ rm(list=ls())
 # INPUT - gcp directory for project- assumes metadata files are present
 # OUTPUT - Rdat file to use in plotting
 
+#!!!! NEEDS ATTENTION ####
+# MAP is not working yet
+# what do do about text files for early deployments 
+# LAT/LON are switched
+# how do you want the labels on the gantt and map? include lookup table
 
 # LOAD LIBRARIES ####
 library(stringr)    
@@ -23,11 +28,10 @@ library(rnaturalearthdata)
 # get directories from NCEI PAM map viewer
 DC = Sys.Date()
 typ = "audio"
-gcpDir  = "gs://noaa-passive-bioacoustic/onms/audio" #ONMS
-gcpDir2 = "gs://noaa-passive-bioacoustic/sanctsound/audio" #SANCTSOUND
-projectN = "onms" #nrs # set this to deal with different metadata formats
-projectN2 = "sanctsound" #nrs # set this to deal with different metadata formats
-outputDir = "F:\\ONMS//overview"   #"G:\\My Drive\\ActiveProjects\\SANCTSOUND_shared\\ONMS"
+gcpDir  = "gs://noaa-passive-bioacoustic/nrs/audio" #ONMS
+outputDir = "F:\\ONMS//overview"   #UPDATE TO LOCAL COMPUTER
+projectN = "NRS"
+projectN2 = "NRS-early"
 inFile = paste0(outputDir, "//ONMSSound_IndicatorCategories_2024-10-29.xlsx")
 lookup = as.data.frame ( read.xlsx(inFile) )
 colnames(lookup) <- lookup[1, ]  # Set first row as column names
@@ -38,19 +42,19 @@ colnames(lookup)
 #these should be the "monitoring sites" you want to gather information about
 command = "gsutil"
 args =  c("ls", gcpDir)
-subdirs = system2(command, args, stdout = TRUE, stderr = TRUE) 
-command = "gsutil"
-args =  c("ls", gcpDir2)
-subdirs2 = system2(command, args, stdout = TRUE, stderr = TRUE)  
-#paste onms + sanctsound
-subdirsALL = c(subdirs, subdirs2)
+subdirsALL = system2(command, args, stdout = TRUE, stderr = TRUE) 
 
 dirNames  = sapply(strsplit(basename( subdirsALL ), "/"), `[`, 1)
 cat("Processing... ", projectN, length(dirNames), "directories" )
-#read one file
-args = c("ls", "-r", subdirsALL[1])
+
+#read one file-- to see what files are available?
+
+siteIn = 1
+args = c("ls", "-r", subdirsALL[siteIn])
 sFiles = system2(command, args, stdout = TRUE, stderr = TRUE)  
 json_files = grep("\\.json$", sFiles, value = TRUE) #metadata files
+json_files
+
 url = paste0("https://storage.googleapis.com/", gsub ("gs://", '', paste(json_files[1], collapse = "") ) )
 h = curl(url, "r")
 json_content = readLines(url)
@@ -59,100 +63,88 @@ tmp = fromJSON(paste(url, collapse = ""))
 
 # GET INFORMATION FROM METADATA FILES ####
 output = NULL
-for (s in 1:length(subdirsALL) ) { # s=21
+for (s in 1:length(subdirsALL) ) { # s=2
   
-  # read in files
+  ## read in files ####
   args = c("ls", "-r", subdirsALL[s])
   sFiles = system2(command, args, stdout = TRUE, stderr = TRUE)  
   json_files = grep("\\.json$", sFiles, value = TRUE) #metadata files
   cat("Processing... ", dirNames[s], "[", s, " of ", length(dirNames),"]", "\n" )
   
-  if ( length(grep(projectN, subdirsALL[s]) ) > 0 ) { # check for format - onms
+  for (jf in 1:length( json_files) ) { # jf = 1
     
-    for (jf in 1:length( json_files) ) {
+    url = paste0("https://storage.googleapis.com/", gsub ("gs://", '', paste(json_files[jf], collapse = "") ) )
+    h = curl(url, "r")
+    json_content = readLines(url)
+    close(h)
+    tmp = fromJSON(paste(url, collapse = ""))
+    
+    ## check which version ####
+    if ( is.null( tmp$deployment$audio_start) ) { #not small caps
       
-      url = paste0("https://storage.googleapis.com/", gsub ("gs://", '', paste(json_files[jf], collapse = "") ) )
-      h = curl(url, "r")
-      json_content = readLines(url)
-      close(h)
-      tmp = fromJSON(paste(url, collapse = ""))
-      
-      name  = tmp$DATA_COLLECTION_NAME 
+      name  = tmp$SITE 
       instr = tmp$INSTRUMENT_TYPE
       start = as.Date( gsub("T"," ", tmp$DEPLOYMENT$AUDIO_START), format = "%Y-%m-%d")
       end   = as.Date( gsub("T"," ", tmp$DEPLOYMENT$AUDIO_END), format = "%Y-%m-%d")
       lat   = tmp$DEPLOYMENT$DEPLOY_LAT
-      lon   = tmp $DEPLOYMENT$DEPLOY_LON
+      lon   = tmp$DEPLOYMENT$DEPLOY_LON
       
       #save to output data - each deployment
       output = rbind(output, c(subdirsALL[s], jf, name, instr, 
                                as.character(start), as.character(end), 
                                lat, lon) )
       
-    }
-    
-    
-  } else if ( length(grep(projectN2, subdirsALL[s]) ) > 0 ) {  # check for format - sanctsound
-    
-    for (jf in 1:length( json_files) ) {
       
-      url = paste0("https://storage.googleapis.com/", gsub ("gs://", '', paste(json_files[jf], collapse = "") ) )
-      h = curl(url, "r")
-      json_content = readLines(url)
-      close(h)
-      tmp = fromJSON(paste(url, collapse = ""))
+    } else if ( !is.null( tmp$deployment$audio_start) ) { #small caps
       
-      
-      name  = tmp$DEPLOYMENT_NAME  
-      instr = tmp$INSTRUMENT_NAME
-      if ( length(instr ) == 0 ){
-        instr = tmp$INSTRUMENT_TYPE
-        start = as.Date( gsub("T"," ", tmp$DEPLOYMENT$AUDIO_START), format = "%Y-%m-%d")
-        end   = as.Date( gsub("T"," ", tmp$DEPLOYMENT$AUDIO_END), format = "%Y-%m-%d")
-        lat   = tmp$DEPLOYMENT$DEPLOY_LAT
-        lon   = tmp $DEPLOYMENT$DEPLOY_LON
-        
-      } else {
-        range = tmp$DATA_QUALITY$'1'$`Date Range`
-        start = as.Date( strsplit(range, " to ")[[1]],format = "%Y-%m-%d" )[1]
-        end   = as.Date( strsplit(range, " to ")[[1]],format = "%Y-%m-%d" )[2] 
-        lon   = tmp$LOCATION$lon
-        lat   = tmp$LOCATION$lat
-        
-      }
+      name  = tmp$site 
+      instr = tmp$instrument_type
+      start = as.Date( gsub("T"," ", tmp$deployment$audio_start), format = "%Y-%m-%d")
+      end   = as.Date( gsub("T"," ", tmp$recover$audio_end), format = "%Y-%m-%d")
+      lat   = tmp$deployment$lat
+      lon   = tmp $deployment$lon
       
       #save to output data - each deployment
       output = rbind(output, c(subdirsALL[s], jf, name, instr, 
                                as.character(start), as.character(end), 
                                lat, lon) )
+    } else {
+      cat(subdirsALL[s], "- Not correct format", "\n" )
       
-    }
+      output = rbind(output, c(subdirsALL[s], jf, NA, NA, 
+                               NA, NA, 
+                               NA, NA) )
+      
+    } 
   }
 }
+
 
 # SAVE SUMMARY ####
 output = as.data.frame(output)
 colnames(output) = c("Path", "DeploymentNumber", "DeploymentName", "Instrument", "Start_Date", "End_Date","Lat","Lon")
 output$Site = basename((output$Path))
-output$Start_Date <- as.Date(output$Start_Date, format = "%Y-%m-%d")
-output$End_Date <- as.Date(output$End_Date, format = "%Y-%m-%d")
+output$Start_Date = as.Date(output$Start_Date, format = "%Y-%m-%d")
+output$End_Date = as.Date(output$End_Date, format = "%Y-%m-%d")
 
 #add site names from look-up colnames(lookup)
 # load(file = paste0(outputDir, "\\GCPaudio_ONMS_gantt_", DC, ".Rda") )
 # unique(lookup$`NCEI ID`)
-matched_data = merge(output, lookup, by.x = "Site", by.y = "NCEI ID", all.x = TRUE)
+#matched_data = merge(output, lookup, by.x = "Site", by.y = "NCEI ID", all.x = TRUE)
 # matched_data = output %>%   left_join(lookup, by = c("Site" = "NCEI ID"))
-output$Region = matched_data$Region
-output$Identifer = matched_data$`Common Name/Identifers`
-output$Description = matched_data$`Site Description/Driver for Monitoring Location Choice`
+#output$Region = matched_data$Region
+#output$Identifer = matched_data$`Common Name/Identifers`
+#output$Description = matched_data$`Site Description/Driver for Monitoring Location Choice`
 output$Duration = difftime( output$End_Date, output$Start_Date,"days")
-colnames(output)
+#colnames(output)
 #remove non-monitoring sites
-outputT = output[!is.na(output$Region),] 
-outputTt = outputT[,c(1,10, 9, 3:6, 13, 7:8, 11:12) ]
+#outputT = output[!is.na(output$Region),] 
+#outputTt = outputT[,c(1,10, 9, 3:6, 13, 7:8, 11:12) ]
+outputT = output
+outputTt = output
 
-save(outputTt, file = paste0(outputDir, "\\data_gantt_ONMS_gantt_", DC, ".Rda") )
-write.csv(outputTt, file = paste0(outputDir, "\\data_gantt_ONMS_gantt_", DC, ".csv") )
+save(outputTt, file = paste0(outputDir, "\\data_gantt_", projectN, "_gantt_", DC, ".Rda") )
+write.csv(outputTt, file = paste0(outputDir, "\\data_gantt_", projectN, "_gantt_", DC, ".csv") )
 colnames(outputTt)
 
 #reformat for per site- total recordings 
@@ -164,19 +156,20 @@ outputMap =  as.data.frame(
       min_start_date = min(Start_Date, na.rm = TRUE)  # Getting the minimum start date for each site
     )
 )
-lookupT = lookup[!is.na(lookup$`NCEI ID`),] 
+#lookupT = lookup[!is.na(lookup$`NCEI ID`),] 
 outputMap$Site = as.character(outputMap$Site)
-lookupT$`NCEI ID` <- as.character(lookupT$`NCEI ID`)
-outputMap2 <- merge(outputMap, lookupT, by.x = "Site", by.y = "NCEI ID", all.x = TRUE)
-colnames ( outputMap2)
-outputMap2a = outputMap2[,c(4,6, 1:3,8:9) ]
-colnames ( outputMap2a)
-colnames(outputMap2a) = c("Region","Sanctuary", "Site",
-                         "TotalDays","StartDate","Latitude", "Longitude") 
-outputMap2a = na.omit(outputMap2a)
+#lookupT$`NCEI ID` <- as.character(lookupT$`NCEI ID`)
+#outputMap2 <- merge(outputMap, lookupT, by.x = "Site", by.y = "NCEI ID", all.x = TRUE)
+#colnames ( outputMap2)
+#outputMap2a = outputMap2[,c(4,6, 1:3,8:9) ]
+#colnames ( outputMap2a)
+#colnames(outputMap2a) = c("Region","Sanctuary", "Site",
+                         # "TotalDays","StartDate","Latitude", "Longitude") 
+#outputMap2a = na.omit(outputMap2a)
 
-save(outputMap2a, file = paste0(outputDir, "\\map_ONMS_map_", DC, ".Rda") )
-write.csv( outputMap2a, file = paste0(outputDir, "\\map_ONMS_map_", DC, ".csv") )
+outputMap2a = outputMap
+save(outputMap2a, file = paste0(outputDir, "\\map_", projectN, "_map_", DC, ".Rda") )
+write.csv( outputMap2a, file = paste0(outputDir, "\\map_", projectN, "_map_", DC, ".csv") )
 
 # GANTT PLOT  ####
 uColors = unique(output$Instrument)
@@ -186,6 +179,11 @@ if (projectN == "onms"){
     "SoundTrap 600" = "#CC6677",#88CCEE",  
     "SoundTrap 300" = "#44AA99", 
     "HARP" =          "#DDCC77") #DDCC77
+} else if (projectN == "NRS") {
+  instrument_colors <- c(
+    "NRS" = "#88CCEE",  
+    "AUH" = "#CC6677",#88CCEE",  
+    "HARP" = "#44AA99") 
 }
 
 
@@ -200,7 +198,7 @@ pT = ggplot(output, aes(y = Site, x = Start_Date, xend = End_Date)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12),
         axis.text.y = element_text(angle = 10, size = 12))
 pT
-ggsave(filename = paste0(outputDir, "/gantt_ONMS_", DC, ".jpg"), plot = pT, width = 8, height = 6, dpi = 300)
+ggsave(filename = paste0(outputDir, "/gantt_", projectN, "_", DC, ".jpg"), plot = pT, width = 8, height = 6, dpi = 300)
 
 # Assuming your data frame is named 'data'
 # Convert the data frame to an sf object for mapping
